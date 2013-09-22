f5424b89f8bd44566534accf01f2b9e1b4991f60
Danijel Maricic <danijel.maricic@ferratum.com>
2013-09-22 14:18:05 +0300

    addedd flash msg

--- api/controllers/DashController.js
+++ api/controllers/DashController.js
@@ -10,18 +10,18 @@ module.exports = {
 	index: function (req, res)
 	{
 		var user = req.user;
-		console.log(req.user);
+		// console.log(req.user);
 		Application.find(
 		{
 			userID: user.id
 		}).done(function (err, _apps)
 		{
-			apps = _apps;
+			return res.view(
+			{
+				apps: _apps,
+				user: user
+			});
 		});
-		return res.view(
-		{
-			apps: apps,
-			user: user
-		});
+
 	}
 };
